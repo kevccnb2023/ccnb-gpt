8a78a566fdf879e55a4db50ae1bc01669f5d9af6
kev <kev@localdomain>
2023-04-24 18:22:29 -0300

    renamed wake word porcupine

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    WAKE_WORD = "terminator"
+    WAKE_WORD = "porcupine"
     db_file = "./users.db"
     db_connection = UserDB(db_file)
     speech_recognizer = SpeechRecognizer("en-US")
